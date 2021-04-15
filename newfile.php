c05b2bf3383bf3424a0a92180b7fb41b64feae31
SuperNath <spk.nth@gmail.com>
2021-04-15 11:03:59 +0530

    table created

--- newfile.php
+++ newfile.php
@@ -1,4 +1,18 @@
 <?php 
 
-echo "hello SuperMan. How r u?";
-echo "created new branch.";
+$arr = array(array("Vivek","7894561235","Developer"),array("Arun","5512244635","Manager"),array("Javed","9988547856","Assistant"));
+
+?>
+
+<table>
+	<tr>
+		<th>Name</th>
+		<th>Phone</th>
+		<th>Designation</th>
+	</tr>
+	<?php foreach($arr as $value): foreach($value as $val): ?>
+	<tr>
+		<td><?php echo $val; ?></td>
+	</tr>
+	<?php endforeach;endforeach; ?>
+</table>
